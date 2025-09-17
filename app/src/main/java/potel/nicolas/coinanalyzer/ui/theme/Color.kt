142a957d48c3adee5a6c2243a21a89e21f337de0
Nicolas Potel <potelnicolasperso@gmail.com>
2025-09-17 14:40:48 +0200

    feat: application theme & font

--- app/src/main/java/potel/nicolas/coinanalyzer/ui/theme/Color.kt
+++ app/src/main/java/potel/nicolas/coinanalyzer/ui/theme/Color.kt
@@ -2,10 +2,29 @@ package potel.nicolas.coinanalyzer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+data class ApplicationTheme(
+    val background: Color,
+    val font: Color,
+    val fontSecondary: Color,
+
+    val increase : Color,
+    val decrease : Color,
+
+    val primary : Color,
+    val secondary : Color,
+    val tertiary: Color
+)
+
+val darkApplicationTheme = ApplicationTheme(
+    background = Color(0xFF111315),
+    font = Color(0xFFFFFFFF),
+    fontSecondary = Color(0xFF333536),
+
+    increase = Color(0xFF007C38),
+    decrease = Color(0xFFC20306),
+
+    primary = Color(0xFF9338F5),
+    secondary = Color(0xFFFC17D4),
+    tertiary = Color(0xFF252728)
+)
