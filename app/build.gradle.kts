04e48e7109bb6a3bdd43856d93cccd3ae633e1b7
Nicolas Potel <nicolas@potel.dev>
2025-11-27 13:38:44 +0100

    feat: starting filters page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
 
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    implementation(libs.androidx.compose.foundation)
     ksp(libs.room.compiler)
 
     testImplementation(libs.junit)
--- app/src/main/java/potel/nicolas/coinanalyzer/pages/Search.kt
+++ app/src/main/java/potel/nicolas/coinanalyzer/pages/Search.kt
@@ -3,25 +3,36 @@ package potel.nicolas.coinanalyzer.pages
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import potel.nicolas.coinanalyzer.R
 import potel.nicolas.coinanalyzer.api.CryptoViewModel
 import potel.nicolas.coinanalyzer.components.SearchBar
 import potel.nicolas.coinanalyzer.components.SearchedCrypto
 import potel.nicolas.coinanalyzer.components.SectionTitle
+import potel.nicolas.coinanalyzer.config.Routes
 import potel.nicolas.coinanalyzer.recentsearch.RecentSearchViewModel
+import potel.nicolas.coinanalyzer.ui.theme.applicationTheme
 
 @Composable
 fun SearchPage(
+    navController: NavHostController,
     cryptoViewModel: CryptoViewModel,
     recentSearchViewModel: RecentSearchViewModel
 ) {
@@ -35,10 +46,29 @@ fun SearchPage(
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        SearchBar(
-            value = searchText,
-            onChange = { searchText = it }
-        )
+        Row(
+            modifier = Modifier,
+        ) {
+            SearchBar(
+                value = searchText,
+                onChange = { searchText = it },
+                modifier = Modifier.weight(1f)
+            )
+
+            IconButton(
+                onClick = { navController.navigate(Routes.FILTERS) },
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.tune),
+                    contentDescription = "Filtering icon",
+                    tint = applicationTheme.font,
+                    modifier = Modifier
+                        .size(26.dp)
+                )
+            }
+        }
         FlowRow(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
